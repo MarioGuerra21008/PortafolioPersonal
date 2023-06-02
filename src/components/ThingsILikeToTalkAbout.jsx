646e8c92066eec98b6fc4aff246a0bb1e554a527
Mario Guerra <gue21008@uvg.edu.gt>
2023-06-02 16:33:43 -0600

    Revert "Final commit"
    This reverts commit 283c8e3fd95d4a239ee1d66350a323cc685fd0d7.

--- src/components/ThingsILikeToTalkAbout.jsx
+++ src/components/ThingsILikeToTalkAbout.jsx
@@ -7,12 +7,12 @@ function Things() {
             <h1 className="title">Cosas que me gustan</h1>
             <p className="subtitle">Para conocerme un poco mejor, acá algunas cosas que me han marcado a lo largo de mi vida :)</p>
             <div className="ILikeThis">
-                <img src="persona.avif" alt="Persona" className="imgThings"></img>
-                <img src="supermb.jpg" alt="Super Mario" className="imgThings"></img>
-                <img src="futbol.jpg" alt="Fulbo" className="imgThings"></img>
-                <img src="gorillaz.jpg" alt="Gorillaz" className="imgThings"></img>
-                <img src="starwars.jpg" alt="Star Wars" className="imgThings"></img>
-                <img src="inuyasha.png" alt="Inuyasha" className="imgThings"></img>
+                <img src="/persona.avif" alt="Persona" className="imgThings"></img>
+                <img src="/supermb.jpg" alt="Super Mario" className="imgThings"></img>
+                <img src="/futbol.jpg" alt="Fulbo" className="imgThings"></img>
+                <img src="/gorillaz.jpg" alt="Gorillaz" className="imgThings"></img>
+                <img src="/starwars.jpg" alt="Star Wars" className="imgThings"></img>
+                <img src="/inuyasha.png" alt="Inuyasha" className="imgThings"></img>
             </div>
         </div>
     )
