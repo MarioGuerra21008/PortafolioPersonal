70241155c8752e622ed0e60961b20aea1547c458
Mario Guerra <gue21008@uvg.edu.gt>
2023-06-02 12:20:33 -0600

    Things That I Like

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -6,7 +6,7 @@ function Banner() {
         <div className="Banner">
             <h1 className="title">Mario Guerra's crib!</h1>
             <h4 className="subtitle">As seen on MTV! <br />(MarioTV) *ba dum tss*</h4>
-            <img src="/murdoc.gif" alt="welcome to my crib" className="giffygif"></img>
+            <img src="/murdoc.gif" alt="welcome to my crib" className="giffygif" width="300px" height="200px"></img>
         </div>
     )
 }
--- src/components/ThingsILikeToTalkAbout.jsx
+++ src/components/ThingsILikeToTalkAbout.jsx
@@ -4,7 +4,16 @@ import './ThingsILikeToTalkAbout.css'
 function Things() {
     return(
         <div className="Things">
-            
+            <h1 className="title">Cosas que me gustan</h1>
+            <p className="subtitle">Para conocerme un poco mejor, acá algunas cosas que me han marcado a lo largo de mi vida :)</p>
+            <div className="ILikeThis">
+                <img src="/persona.avif" alt="Persona" className="imgThings"></img>
+                <img src="/supermb.jpg" alt="Super Mario" className="imgThings"></img>
+                <img src="/futbol.jpg" alt="Fulbo" className="imgThings"></img>
+                <img src="/gorillaz.jpg" alt="Gorillaz" className="imgThings"></img>
+                <img src="/starwars.jpg" alt="Star Wars" className="imgThings"></img>
+                <img src="/inuyasha.png" alt="Inuyasha" className="imgThings"></img>
+            </div>
         </div>
     )
 }
